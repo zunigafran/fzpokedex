8fa6c98f4b77fe535e3bc210646dbf120d8947b0
zunigafran <fzuniga325@gmail.com>
2023-10-23 11:01:23 -0500

    "new changes"

--- src/components/Dex.js
+++ src/components/Dex.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config'
 
-function Dex() {
+export default function Dex () {
+  useEffect(() => {
+    axios.get(POKEMON_API_URL + "?limit=151").then((response) => {
+      console.log(reponse.data)
+    })
+  }, [])
+
   return (
-    <h1>Hello World</h1>
+    <h1>Dex</h1>
   )
 }
-
-export default Dex
