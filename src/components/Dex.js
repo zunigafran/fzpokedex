a676090bf1d9bf533b9bc4611d1de49a25bb1caa
zunigafran <fzuniga325@gmail.com>
2023-10-23 11:06:53 -0500

    add build

--- src/components/Dex.js
+++ src/components/Dex.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
-import { IMAGE_API_URL, POKEMON_API_URL } from '../config'
+import { POKEMON_API_URL } from '../config'
 
 export default function Dex () {
   useEffect(() => {
     axios.get(POKEMON_API_URL + "?limit=151").then((response) => {
-      console.log(reponse.data)
+      console.log(response.data)
     })
   }, [])
 
