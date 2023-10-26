df6aa4d9cec3db756290441832f80c390de64165
zunigafran <fzuniga325@gmail.com>
2023-10-26 10:38:07 -0500

    search

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div id="search">
+    <div className="p-4" id="search-bar">SearchBar</div>
+    <div id="search-results">SearchResults</div>
+    </div>
+  )
+}
+
+export default Search
